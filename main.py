36344d994e719b33c8a71f406b6e43ab23ff187d
0rphee <0rph3e@proton.me>
2023-05-13 23:24:12 -0600

    add classes for each class
    subject:
      name: poo
      professor: prof1
      classes: [ class:
                   day: monday
                   interval: (07:00 - 8:30)
               , class:
                   day: friday
                   interval: (07:00 - 8:30)
               ]

--- main.py
+++ main.py
@@ -1,7 +1,35 @@
 
+
+class Time:
+    def __init__(self, hour: int, minute: int):
+        self.hour = hour
+        self.minute = minute
+
+
+class Interval:
+    def __init__(self, start_time: Time, end_time: Time):
+        self.start_time = start_time
+        self.end_time = end_time
+
+
+class Class:
+    def __init__(self, day: str, interval: Interval):
+        self.day = day
+        self.interval = interval
+
+
+class Subject:
+    def __init__(self, name: str, professor: str, classes: [Class]):
+        self.name = name
+        self.professor = professor
+        self.classes = classes
+
+
 def main():
-	pass
+    pass
 
 
+# execute ONLY if the module is not imported:
+# ex. python3 main.py
 if __name__ == "__main__":
-	main()
+    main()
